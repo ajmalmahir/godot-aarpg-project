f99c7cc9ec476a177e66453bfec4bd3bb9624ea0
unknown <ajmalmahir@icloud.com>
2025-04-16 00:00:00 +0100

    created the enemy_state script

---
+++ Enemies/Scripts/states/enemy_state.gd
@@ -0,0 +1,15 @@
+class_name EnemyState extends Node
+
+
+## Stores a reference to the enemy that this state belongs to
+var enemy : Enemy
+# var state_machine : EnemyStateMachine
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
