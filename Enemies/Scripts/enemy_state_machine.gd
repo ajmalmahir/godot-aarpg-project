03b9ffd04ecff76f61cc39c46da7675590936f4d
unknown <ajmalmahir@icloud.com>
2025-04-17 00:00:00 +0100

    created the enemy_state_machine script and then added the enemy state machine reference to the enemy state script

--- Enemies/Scripts/enemy_state_machine.gd
+++ Enemies/Scripts/enemy_state_machine.gd
@@ -1,9 +1,5 @@
-class_name EnemyState extends Node
+class_name EnemyStateMachine extends Node
 
-
-## Stores a reference to the enemy that this state belongs to
-var enemy : Enemy
-var state_machine : EnemyStateMachine
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
