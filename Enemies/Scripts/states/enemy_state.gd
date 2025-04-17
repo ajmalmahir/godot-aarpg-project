84a63154eb67637bc2f4f95f5c8fcdbf236a8539
unknown <ajmalmahir@icloud.com>
2025-04-18 00:00:00 +0100

    updated enemy_state and enemy_state_machine to initialise properly on startup

--- Enemies/Scripts/states/enemy_state.gd
+++ Enemies/Scripts/states/enemy_state.gd
@@ -5,11 +5,17 @@ class_name EnemyState extends Node
 var enemy : Enemy
 var state_machine : EnemyStateMachine
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func init() -> void:
 	pass
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+	
+func process( _delta : float ) -> EnemyState:
+	return null
+
+func physics( _delta : float ) -> EnemyState:
+	return null
