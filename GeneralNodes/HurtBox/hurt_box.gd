032093546441b9fc615daaaa2e22af4de8235e0d
unknown <ajmalmahir@icloud.com>
2025-05-12 00:00:00 +0100

    cleaned up some parts of the code and updated the hitbox/hurtbox damage code

--- GeneralNodes/HurtBox/hurt_box.gd
+++ GeneralNodes/HurtBox/hurt_box.gd
@@ -4,15 +4,10 @@ class_name HurtBox extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	area_entered.connect( AreaEntered )
+	area_entered.connect( _area_entered )
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func AreaEntered( a : Area2D ) -> void:
+func _area_entered( a : Area2D ) -> void:
 	if a is HitBox:
-		a.TakeDamage( self )
+		a.take_damage( self )
 	pass
